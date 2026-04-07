f15f1a8516a4a64e88cae32b3c878a00fc53221b
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-08 00:25:46 +0800

    diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,5 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
-git clone https://github.com/kenzok8/small.git package/small
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
