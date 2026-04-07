d35b451ed24db70af41228e4cdcb5d1488ad4b29
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-07 20:48:53 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,4 @@ sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 sed -i '3i PKG_FORTIFY_SOURCE=0' package/feeds/packages/libxcrypt/Makefile
+wget -O feeds/kenzo/luci-app-quickstart/luasrc/controller/istore_backend.lua https://gist.githubusercontent.com/puteulanus/1c180fae6bccd25e57eb6d30b7aa28aa/raw/51ae1464e068fadc348c0b577f8ffd338bbd7ee5/istore_backend.lua
