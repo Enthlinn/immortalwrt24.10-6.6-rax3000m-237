63997656ba9e13325fa9eadbda6107ce9d287529
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-07 20:33:05 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,5 @@
 # Modify default IP
 sudo apt install libfuse-dev
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
 sed -i '3i PKG_FORTIFY_SOURCE=0' package/feeds/packages/libxcrypt/Makefile
