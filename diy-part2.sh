3309bdf933cba0643d9e7b79f7e7b1f6a3e93934
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-09 22:18:03 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,4 +21,4 @@ sed -i '3i PKG_FORTIFY_SOURCE=0' package/feeds/packages/libxcrypt/Makefile
 #修复quickstart温度显示
 wget -O package/kenzo/luci-app-quickstart/luasrc/controller/istore_backend.lua https://gist.githubusercontent.com/puteulanus/1c180fae6bccd25e57eb6d30b7aa28aa/raw/51ae1464e068fadc348c0b577f8ffd338bbd7ee5/istore_backend.lua
 #修复ddns-go
-cp ../ddns-go /home/enthlinn/rax3000m/package/kenzo/ddns-go/file/ddns-go.init
+wget -O /home/enthlinn/rax3000m/package/kenzo/ddns-go/file/ddns-go.init https://raw.githubusercontent.com/Enthlinn/immortalwrt24.10-6.6-rax3000m-237/refs/heads/openwrt-24.10-6.6/ddns-go
