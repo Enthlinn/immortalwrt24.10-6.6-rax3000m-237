a3ec06e9a8dad999d848af6d200ac2fbffaeb7d4
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-13 11:09:21 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,13 +12,15 @@
 
 # Modify default IP
 sudo apt install libfuse-dev
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+./scripts/feeds update -a 
+rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
 ./scripts/feeds install -a
-#修复libxcrypt无法编译
-sed -i '3i PKG_FORTIFY_SOURCE=0' package/feeds/packages/libxcrypt/Makefile
+#修复libxcrypt无法编译,好像用不到了
+#sed -i '3i PKG_FORTIFY_SOURCE=0' package/feeds/packages/libxcrypt/Makefile
 #修复quickstart温度显示
 wget -O package/kenzo/luci-app-quickstart/luasrc/controller/istore_backend.lua https://raw.githubusercontent.com/Enthlinn/immortalwrt24.10-6.6-rax3000m-237/refs/heads/openwrt-24.10-6.6/istore_backend.lua
 #修复ddns-go
 wget -O package/kenzo/ddns-go/file/ddns-go.init https://raw.githubusercontent.com/Enthlinn/immortalwrt24.10-6.6-rax3000m-237/refs/heads/openwrt-24.10-6.6/ddns-go
+#修改os-release
+wget -O package/base-files/files/usr/lib/os-release https://raw.githubusercontent.com/Enthlinn/immortalwrt24.10-6.6-rax3000m-237/refs/heads/openwrt-24.10-6.6/os-release
