cd77ee365c4985db9e4179c6ece2e7c6ae5292e3
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-13 21:41:07 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,7 +13,7 @@
 # Modify default IP
 sudo apt install libfuse-dev
 ./scripts/feeds update -a 
-rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang package/jell/dapnet-gateway/
 git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
 ./scripts/feeds install -a
 #修复libxcrypt无法编译
