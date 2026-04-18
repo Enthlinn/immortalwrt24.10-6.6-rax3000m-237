96ba652630cd3ee24dcb64563ae00813d05b5625
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-18 19:57:18 +0800

    diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,10 +16,14 @@ sudo apt install libfuse-dev
 #更新golang,ddns-go
 rm -rf feeds/packages/lang/golang feeds/packages/net/ddns-go feeds/luci/applications/luci-app-ddns-go
 git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
-./scripts/feeds install -a -f
+./scripts/feeds install -a
+#强制更新ddns-go软件包
+./scripts/feeds install -f luci-app-ddns-go ddns-go
+#修复ddns-go无法在升级时保留配置
+echo "/etc/ddns-go" >> package/base-files/files/etc/sysupgrade.conf
 #修复libxcrypt无法编译
 sed -i '3i PKG_FORTIFY_SOURCE=0' package/feeds/packages/libxcrypt/Makefile
 #修复quickstart温度显示
 wget -O package/jell/luci-app-quickstart/luasrc/controller/istore_backend.lua https://raw.githubusercontent.com/Enthlinn/immortalwrt24.10-6.6-rax3000m-237/refs/heads/openwrt-24.10-6.6/istore_backend.lua
-#修复ddns-go
+#修复ddns-go无法从luci启动
 wget -O package/jell/ddns-go/file/ddns-go.init https://raw.githubusercontent.com/Enthlinn/immortalwrt24.10-6.6-rax3000m-237/refs/heads/openwrt-24.10-6.6/ddns-go.init
