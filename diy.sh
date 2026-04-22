a926bb4b98991f447a2341fad929f454ff9e8179
Enthlinn <47232016+Enthlinn@users.noreply.github.com>
2026-04-22 20:47:30 +0800

    通过自定义命令添加和修复软件包

--- diy.sh
+++ diy.sh
@@ -6,11 +6,16 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 #在这里添加想要的软件源吧
 #建议先搜索rax3000m.config文件确认是否默认源里已经存在想要的软件包
+#如果存在想要的软件包比如openclash
+#可以直接在文件中将# CONFIG_PACKAGE_luci-app-openclash is not set改成CONFIG_PACKAGE_luci-app-openclash=y
+#注意别忘了去掉前面的#
+#理论上就可以直接编译了
+#如果改了之后编译出来的固件中没有那个软件包就是依赖检查过不去被自动移除了
+#也可以直接使用sed修改,比如下面这行代码
+#sed -i 's|# CONFIG_PACKAGE_luci-app-openclash is not set|CONFIG_PACKAGE_luci-app-openclash=y|g' .config
 #更新golang
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
@@ -25,9 +30,6 @@ cp -r ../jell/ddns-go package/jell/ddns-go
 cp -r ../jell/luci-app-ddns-go package/jell/luci-app-ddns-go
 #其实jell源里也有watchdog，但我懒得改了
 git clone https://github.com/sirpdboy/luci-app-watchdog.git package/watchdog
-#强制更新ddns-go软件包,好像并不需要直接编译就行
-#./scripts/feeds install -f luci-app-ddns-go
-#./scripts/feeds install -f ddns-go
 #修复ddns-go无法在升级时保留配置
 echo "/etc/ddns-go" >> package/base-files/files/etc/sysupgrade.conf
 #修复libxcrypt无法编译
